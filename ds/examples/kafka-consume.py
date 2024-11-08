a39f41d421d87e58d3232d28c8ec3eb351a97e15
Lauri <huhta.lauri@gmail.com>
2024-11-08 18:41:02 +0200

    kafka examples

--- ds/examples/kafka-consume.py
+++ ds/examples/kafka-consume.py
@@ -1,22 +1,35 @@
 from confluent_kafka import Consumer
+from argparse import ArgumentParser
+import json
 
 c = Consumer({
-    'bootstrap.servers': '100.82.107.137',
+    'bootstrap.servers': 'datanautti',
     'group.id': 'mygroup',
     'auto.offset.reset': 'earliest'
 })
 
-c.subscribe(['testtopic'])
 
-while True:
-    msg = c.poll(1.0)
+def consume(topic: str):
+    c.subscribe([topic])
 
-    if msg is None:
-        continue
-    if msg.error():
-        print("Consumer error: {}".format(msg.error()))
-        continue
+    while True:
+        msg = c.poll(0.2)
 
-    print('Received message: {}'.format(msg.value().decode('utf-8')))
+        if msg is None:
+            continue
+        if msg.error():
+            print("Consumer error: {}".format(msg.error()))
+            continue
 
-c.close()
+        print(json.loads(msg.value().decode("utf-8")))
+        row = json.loads(msg.value().decode("utf-8"))
+        print("ts", row["timestamp"])
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+
+    parser.add_argument("--topic", "-t", type=str, required=True)
+
+    args = parser.parse_args()
+
+    consume(topic=args.topic)
--- ds/examples/kafka-produce.py
+++ ds/examples/kafka-produce.py
@@ -1,7 +1,9 @@
 from confluent_kafka import Producer
 from time import sleep
+from argparse import ArgumentParser
+import json
 
-p = Producer({'bootstrap.servers': '100.82.107.137'})
+p = Producer({'bootstrap.servers': 'datanautti'})
 
 def delivery_report(err, msg):
     """ Called once for each message produced to indicate delivery result.
@@ -11,11 +13,20 @@ def delivery_report(err, msg):
     else:
         print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
-for i in range(10000):
-    p.produce('testtopic', f"foo{i}", callback=delivery_report)
-    p.flush()
-    sleep(3)
+def produce(topic: str):
+    for i in range(10000):
+        # p.produce('testtopic', f"foo{i}", callback=delivery_report)
+        p.produce(topic, json.dumps({"msg": "foo", "num": i}), callback=delivery_report)
+        p.flush()
+        sleep(3)
 
 
+if __name__ == "__main__":
+    parser = ArgumentParser()
 
-# Wait for any outstanding messages to be delivered and delivery report
+    parser.add_argument("--topic", "-t", type=str, required=True)
+
+    args = parser.parse_args()
+
+    produce(topic=args.topic)
+
